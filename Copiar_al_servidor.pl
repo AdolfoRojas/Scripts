15ec86b8eafc88eb4f338105b112f1a0d5132982
Adolfo Alexis Rojas Hidalgo <46684987+AdolfoRojas@users.noreply.github.com>
2020-05-16 14:44:06 -0400

    Delete Copiar_al_servidor.pl

--- Copiar_al_servidor.pl
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/perl
-print "Ingrese Archivo a copiar\n";
-$file1=<>;
-chomp ($file1);
-print "Ingrese ruta y nombre de destino\n";
-$file2=<>;
-chomp ($file2); 
-system ("scp -P 1313 $file1 adolforojas\@200.89.65.156:/home/adolforojas/$file2");
-#adolfolib
