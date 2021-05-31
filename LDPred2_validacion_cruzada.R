6b59fd0832029a9f6bc527482b44a99631e42eb1
AdolfoRojas <adolfo_1995@hotmail.cl>
2021-05-31 12:57:18 -0400

    sgrtgtgstg

--- LDPred2_validacion_cruzada.R
+++ LDPred2_validacion_cruzada.R
@@ -6,6 +6,7 @@ library(telegram.bot)
 bot = Bot(token = bot_token("AARH_95_bot"))
 chat_id <- bot_token("chat_id")
 library(ggpubr)
+Taruca_adolfo_tesis <- "adolfo@200.89.65.156:/run/media/vinicius/run-projects/Adolfo/Resultados_Tesis/Objetivo_1/"
 #################################################################################################
 system("rm *.rds")
 system("rm *.bk")
@@ -22,9 +23,7 @@ sumstats <- bigreadr::fread2("GWAS_PanUKBB_final.tsv") # Read external summary s
 set.seed(1)
 ind.final.val <- sample(nrow(G), 258)
 poblacion_restante <- setdiff(rows_along(G), ind.final.val)
-#~~~
 ## **Matching variants between genotype data and summary statistics**
-#~~~
 sumstats$n_case <- 13257
 sumstats$n_control <- 205913
 sumstats$n_eff <- 4 / (1 / sumstats$n_case + 1 / sumstats$n_control)
@@ -56,8 +55,6 @@ for(semilla in 1:10){
 message_to_bot <- paste('K = ', semilla, sep = " ")
 bot$sendMessage(chat_id, text = message_to_bot)
 set.seed(semilla)
-#ind.val <- sample(poblacion_restante, 1500)
-#ind.test <- setdiff(poblacion_restante, ind.val)
 ind.test <- poblacion_restante[(1+200*(semilla-1)):(200+200*(semilla-1))]
 ind.val <- setdiff(poblacion_restante, ind.test)
 
@@ -83,8 +80,6 @@ h2_est <- ldsc[["h2"]]
 beta_inf <- snp_ldpred2_inf(corr, df_beta, h2 = h2_est)
 pred_inf <- big_prodVec(G, beta_inf, ind.row = ind.test, ind.col = info_snp$`_NUM_ID_`, ncores = NCORES)
 AUC_inf <- AUCBoot(pred_inf, y[ind.test], seed = 1)
-#message_to_bot <- paste("AUCinf =", AUC_inf[1], "SD =", AUC_inf[4], sep = " ")
-#bot$sendMessage(chat_id, text = message_to_bot)
 ################################################################################################
 h2_seq <- round(h2_est * c(0.7, 1, 1.4), 4)
 p_seq <- signif(seq_log(1e-4, 1, length.out = 17), 2)
@@ -117,8 +112,7 @@ Grid_plot <- ggplot(params, aes(x = p, y = score, color = as.factor(h2))) +
 
 pred_nosp <- big_prodVec(G, best_grid_nosp, ind.row = ind.test, ind.col = df_beta[["_NUM_ID_"]])
 AUC_pred_nosp <- AUCBoot(pred_nosp, y[ind.test], seed = 1)
-#message_to_bot <- paste("AUCgrid-nosp =", AUC_pred_nosp[1], "SD =", AUC_pred_nosp[4], sep = " ")
-#bot$sendMessage(chat_id, text = message_to_bot)
+
 best_grid_sp <- params %>%
   mutate(id = row_number()) %>%
   filter(sparse) %>%
@@ -129,8 +123,6 @@ best_grid_sp <- params %>%
 
 pred_sp <- big_prodVec(G, best_grid_sp, ind.row = ind.test, ind.col = df_beta[["_NUM_ID_"]])
 AUC_pred_sp <- AUCBoot(pred_sp, y[ind.test], seed = 1)
-#message_to_bot <- paste("AUCgrid-sp =", AUC_pred_sp[1], "SD =", AUC_pred_sp[4], sep = " ")
-#bot$sendMessage(chat_id, text = message_to_bot)
 ################################################################################################
 multi_auto <- snp_ldpred2_auto(corr, df_beta, h2_init = h2_est, vec_p_init = seq_log(1e-4, 0.9, length.out = NCORES), ncores = NCORES)
 auto <- multi_auto[[1]]
@@ -152,8 +144,7 @@ keep <- abs(sc - median(sc)) < 3 * mad(sc)
 final_beta_auto <- rowMeans(beta_auto[, keep])
 final_pred_auto <- rowMeans(pred_auto[, keep])
 AUC_Auto <- AUCBoot(final_pred_auto, y[ind.test], seed = 1)
-#message_to_bot <- paste("AUCauto =", AUC_Auto[1], "SD =", AUC_Auto[4], sep = " ")
-#bot$sendMessage(chat_id, text = message_to_bot)
+
 nam_best_grid_sp <- paste("best_grid_sp_K", semilla, sep = "")
 assign(nam_best_grid_sp, best_grid_sp)
 
@@ -187,7 +178,7 @@ full_list <- rbind(full_list, data.frame(semilla,
 system("rm tmp-data/*.sbk")
 }
 write.table(K_betas, sep = "\t", file ="Efectos_beta_validacion_cruzada.tsv", row.names = F, quote = F, col.names = T)
-system("scp Efectos_beta_validacion_cruzada.tsv adolfo@200.89.65.156:/run/media/vinicius/run-projects/Adolfo/Efectos_beta_validacion_cruzada.tsv")
+system("scp Efectos_beta_validacion_cruzada.tsv adolfo@200.89.65.156:/run/media/vinicius/run-projects/Adolfo/Resultados_Tesis/Objetivo_1/Efectos_beta_validacion_cruzada.tsv")
 
 info_snp$best_grid_nosp_mean_K <- rowMeans(K_betas[grepl("best_grid_nosp_K",names(K_betas))])
 info_snp$best_grid_sp_mean_K <- rowMeans(K_betas[grepl("best_grid_sp_K",names(K_betas))])
@@ -217,7 +208,89 @@ colnames(full_list) <- c("K","Infinitesimal", "Grid-NOSP", "Grid-SP", "Automatic
 write.table(full_list, sep = "\t", file ="Rendimientos_validacion_cruzada.tsv", row.names = F, quote = F, col.names = T)
 bot$sendDocument(chat_id, document = "Rendimientos_validacion_cruzada.tsv")
 save.image(file = "2_entorno_validacion_cruzada.RData")
+###################################################################################################################################
+# Analisis resultados promedio
+## eleccion mejor modelo en base a AUC mayor
+if (AUC_pred_sp[[1]] > AUC_inf[[1]] & AUC_pred_sp[[1]] > AUC_pred_nosp[[1]] & AUC_pred_sp[[1]] > AUC_Auto[[1]]) {
+   best_auc_name <- "Grid SP model"
+   best_auc_name2 <- "Grid_SP_model"
+   best_auc_value <- AUC_pred_sp[[1]]
+   best_auc_pred <- pred_sp
+} else if (AUC_pred_nosp[[1]] > AUC_inf[[1]] & AUC_pred_nosp[[1]] > AUC_pred_sp[[1]] & AUC_pred_nosp[[1]] > AUC_Auto[[1]]) {
+   best_auc_name <- "Grid NOSP model"
+   best_auc_name2 <- "Grid_SP_model"
+   best_auc_value <- AUC_pred_nosp[[1]]
+   best_auc_pred <- pred_nosp
+} else if (AUC_Auto[[1]] > AUC_inf[[1]] & AUC_Auto[[1]] > AUC_pred_nosp[[1]] & AUC_Auto[[1]] > AUC_pred_sp[[1]]) {
+   best_auc_name <- "Automatic model"
+   best_auc_name2 <- "Automatic_model"
+   best_auc_value <- AUC_Auto[[1]]
+   best_auc_pred <- final_pred_auto
+} else if (AUC_inf[[1]] > AUC_pred_sp[[1]] & AUC_inf[[1]] > AUC_pred_nosp[[1]] & AUC_inf[[1]] > AUC_Auto[[1]]) {
+   best_auc_name <- "Infinitesimal model"
+   best_auc_name2 <- "Infinitesimal_model"
+   best_auc_value <- AUC_inf[[1]]
+   best_auc_pred <- pred_inf
+}  
+## VEP Analisis
+non_zero_grid_sp <- as.data.frame(info_snp[info_snp$best_grid_sp_mean_K != 0,]$rsid)
+names(non_zero_grid_sp) <- "rsid"
+df2 <- info_snp
+DF3 <- df2[c("chr","pos","a1","a0","rsid")]
+DF3$alelos <- paste(DF3$a1, DF3$a0, sep = "/")
+DF3$pos2 <- DF3$pos
+DF3$strand <- NA
+DF3 <- DF3[c("chr","pos","pos2","alelos","strand","rsid")]
+DF3 <- DF3[order(DF3$chr,DF3$pos),]
+DF2 <- merge(DF3, non_zero_grid_sp, by = "rsid")
+DF2 <- DF2[c("chr","pos","pos2","alelos","strand","rsid")]
+DF2 <- DF2[order(DF2$chr,DF2$pos),]
+write.table(DF3,file = paste("p-Value_threshold_", p_value, "_hapmap3_all_variant_effect.txt", sep = ""), quote = F, col.names=F, row.names= F)
+write.table(DF2,file = paste("p-Value_threshold_", p_value, "_hapmap3_all_variant_effect_non_zero.txt", sep = ""), quote = F, col.names=F, row.names= F)
+system(paste("perl /media/storage2/software/ensembl-vep/vep --input_file ", paste("p-Value_threshold_", p_value, "_hapmap3_all_variant_effect_non_zero.txt", sep = ""), " --output_file ", paste("VEP_p-Value_threshold_", p_value, "_hapmap3_all_variant_effect_non_zero", sep = ""), " --cache --regulatory --no_headers --biotype --dir_cache /media/storage2/software/ensembl-vep/cache/ --force_overwrite --verbose --per_gene --symbol --domains --fork 15 --offline --assembly GRCh37", sep = ""))
+system(paste("perl /media/storage2/software/ensembl-vep/vep --input_file ", paste("p-Value_threshold_", p_value, "_hapmap3_all_variant_effect.txt", sep = ""), " --output_file ", paste("VEP_p-Value_threshold_", p_value, "_hapmap3_all_variant_effect", sep = ""), " --cache --regulatory --no_headers --biotype --dir_cache /media/storage2/software/ensembl-vep/cache/ --force_overwrite --verbose --per_gene --symbol --domains --fork 15 --offline --assembly GRCh37", sep = ""))
+system("rm *_warnings.txt")
+system(paste("scp ", paste("VEP_p-Value_threshold_", p_value, "_hapmap3_all_variant_effect_non_zero_summary.html", sep = "")," ", Taruca_adolfo_tesis, paste("VEP_p-Value_threshold_", p_value, "_hapmap3_all_variant_effect_non_zero_summary.html", sep = ""), sep = ""))
+system(paste("scp ", paste("VEP_p-Value_threshold_", p_value, "_hapmap3_all_variant_effect_summary.html", sep = "")," ", Taruca_adolfo_tesis, paste("VEP_p-Value_threshold_", p_value, "_hapmap3_all_variant_effect_summary.html", sep = ""), sep = ""))
+bot$sendDocument(chat_id, document = paste("VEP_p-Value_threshold_", p_value, "_hapmap3_all_variant_effect_non_zero_summary.html", sep = ""))
+bot$sendDocument(chat_id, document = paste("VEP_p-Value_threshold_", p_value, "_hapmap3_all_variant_effect_summary.html", sep = ""))
+system(paste("scp ", paste("VEP_p-Value_threshold_", p_value, "_hapmap3_all_variant_effect_non_zero", sep = "")," ", Taruca_adolfo_tesis, paste("VEP_p-Value_threshold_", p_value, "_hapmap3_all_variant_effect_non_zero", sep = ""), sep = ""))
+system(paste("scp ", paste("VEP_p-Value_threshold_", p_value, "_hapmap3_all_variant_effect", sep = "")," ", Taruca_adolfo_tesis, paste("VEP_p-Value_threshold_", p_value, "_hapmap3_all_variant_effect", sep = ""), sep = ""))
+#bot$sendDocument(chat_id, document = paste("Grid_model_plot_p-value_", p_value,".png", sep = ""))
+#bot$sendDocument(chat_id, document = paste("Auto_model_plot_p-value_", p_value,".png", sep = ""))
+bot$sendDocument(chat_id, document = paste("VEP_p-Value_threshold_", p_value, "_hapmap3_all_variant_effect_non_zero_summary.html", sep = ""))
+bot$sendDocument(chat_id, document = paste("VEP_p-Value_threshold_", p_value, "_hapmap3_all_variant_effect_non_zero", sep = ""))
+# Odds Ratio Analisis
+Prediccion <- as.data.frame(cbind(obj.bigSNP$fam$sample.ID[ind.final.val],y[ind.final.val],best_auc_pred))
+Prediccion$V2 <- as.character(Prediccion$V2)
+Prediccion[Prediccion$V2 == 0,]$V2 <- "Control"
+Prediccion[Prediccion$V2 == 1,]$V2 <- "Case"
+Prediccion$V2 <- as.factor(Prediccion$V2)
+Prediccion$best_auc_pred <- as.character(Prediccion$best_auc_pred)
+Prediccion$best_auc_pred <- as.numeric(Prediccion$best_auc_pred)
+less_risk <- head(Prediccion[order(Prediccion$best_auc_pred),],length(ind.test)*0.95)
+low_risk <- head(Prediccion[order(Prediccion$best_auc_pred),],length(ind.test)*0.05)
+odds_less_risk <- length(less_risk[less_risk$V2 == "Case",]$V2)/length(less_risk[less_risk$V2 == "Control",]$V2)
+greater_risk <- tail(Prediccion[order(Prediccion$best_auc_pred),],length(ind.test)*0.05)
+odds_greater_risk <- length(greater_risk[greater_risk$V2 == "Case",]$V2)/length(greater_risk[greater_risk$V2 == "Control",]$V2)
+odds_ratio <- odds_greater_risk/odds_less_risk
+ggdensity(Prediccion, x = "best_auc_pred", add = "mean", rug = TRUE, color = "V2", fill = "V2", palette = c("#00AFBB", "#E7B800")) +
+    ggtitle("Distribution of test samples PRS", subtitle = paste(best_auc_name, " = ", round(best_auc_value,4), sep = ""))+ 
+    theme(legend.position = c(0.85,0.75), plot.title = element_text(hjust = 0, size=20, vjust= 0), legend.title = element_text(size = 15), legend.text = element_text(size = 12), legend.direction = "horizontal") +
+    ylab("Density")+ xlab("PRS") + labs(fill = "Sample type", color ="Sample type")+ geom_vline(xintercept = greater_risk$best_auc_pred[1], na.rm = FALSE, show.legend = NA)
+
+ggsave(paste("Density_plot_",best_auc_name2,"_p-Value_threshold_", p_value,".png", sep = ""), width = 16, height = 9, dpi = 600, units = "in")
+system(paste("scp ", paste("Density_plot_",best_auc_name2,"_p-Value_threshold_", p_value,".png", sep = "")," ", Taruca_adolfo_tesis, paste("Density_plot_",best_auc_name2,"_p-Value_threshold_", p_value,".png", sep = ""), sep = ""))
+
+ggdotchart(Prediccion, x = "V1", y = "best_auc_pred", color = "V2", dot.size = 0.5, palette = c("#00AFBB", "#E7B800"), sorting = "ascending", ggtheme = theme_pubr()) + theme(axis.text.x=element_blank()) + 
+           ylab("PRS")+ xlab("Test Samples")+ geom_vline(xintercept = greater_risk$V1[1], na.rm = FALSE, show.legend = NA)
+
+ggsave(paste("Scores_vs_samples_p-Value_", p_value,".png", sep = ""), width = 16, height = 9, dpi = 600, units = "in")
+system(paste("scp ", paste("Scores_vs_samples_p-Value_", p_value,".png", sep = "")," ", Taruca_adolfo_tesis, paste("Scores_vs_samples_p-Value_", p_value,".png", sep = ""), sep = ""))
+
+#save.image(file = paste("p-value_", p_value, ".RData"),sep = "")
+
+###################################################################################################################################
 message_to_bot <- "Analisis Finalizado"
 bot$sendMessage(chat_id, text = message_to_bot)
-
 ##################################################################################################################################
